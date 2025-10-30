673ec4f15b67128edbedaf46f968b706ee12273f
ayemyatminnGD2000 <ayemyatminn@goldendragon2000.com>
2025-10-30 16:42:00 +0700

    In progress
    - Change phone number in progress

---
+++ HeyPlay/HeyPlay/Modules/ChangePhoneNumber/SuccessfullyChangePhoneNumberScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SuccessfullyChangePhoneNumberScreen.swift
+//  HeyPlay
+//
+//  Created by Aye Myat Minn on 10/30/25.
+//
+
+import SwiftUI
+
+struct SuccessfullyChangePhoneNumberScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuccessfullyChangePhoneNumberScreen()
+}
---
+++ HeyPlay/HeyPlay/Modules/ChangePhoneNumber/VerifyOtpScreen.swift
@@ -0,0 +1,18 @@
+//
+//  VerifyOtpScreen.swift
+//  HeyPlay
+//
+//  Created by Aye Myat Minn on 10/30/25.
+//
+
+import SwiftUI
+
+struct VerifyOtpScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerifyOtpScreen()
+}
